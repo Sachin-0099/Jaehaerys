17628a0002aead85752b8808786fd9d92263af26
prince.priyadarshi <prince.priyadarshi@agilonhealth.com>
2024-12-27 21:39:11 +0530

    init

--- clean-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf venv
- 
-docker-compose down
-
-docker volume prune -f
-
-sudo rm -rf spark-streaming/scala/project spark-streaming/scala/target ./jars_dir
---
+++ src/main/java/com/jaehaerys/hl7tranfromer/Hl7tranfromerApplication.java
@@ -0,0 +1,13 @@
+package com.jaehaerys.hl7tranfromer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hl7tranfromerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hl7tranfromerApplication.class, args);
+	}
+
+}
